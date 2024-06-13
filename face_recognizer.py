9ae10b638b184da69c3bd4a97e4021bef2c0dcf4
iamawez <awezmanyar1672@gmail.com>
2024-06-13 13:17:12 +0530

    Update

--- face_recognizer.py
+++ face_recognizer.py
@@ -14,7 +14,7 @@ face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('face_trained.yml')
 
 
-img = cv.imread(r'C:\python\opencv\face_recognization\Images\Robert Downey\Robert.jpg')
+img = cv.imread(r'C:\python\opencv\face_recognization\Images\Tom Holland\tom.jpg')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.namedWindow("show", cv.WINDOW_NORMAL)
@@ -31,10 +31,10 @@ for(x,y,w,h) in face_rect:
     faces_roi = gray[y:y+h, x:x+w]
 
     label, confidence = face_recognizer.predict(faces_roi)
-    print(f'Label = {people[label]} with a confidence of {confidence}')
+    # print(f'Label = {people[label]} with a confidence of {confidence}')
 
 
-    cv.putText(img, str(people[label]),  (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=4)
+    cv.putText(img, str(people[label]),  (10,28), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=3)
     cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
 
 
